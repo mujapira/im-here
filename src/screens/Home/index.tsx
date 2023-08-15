d83b8dd726798a020bace32f0e68fea6962c4a11
Mujapira <malmasmello2@gmail.com>
2023-08-15 19:52:29 -0300

    Participants list

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,28 +1,66 @@
-import { Text, TextInput, TouchableOpacity, View } from "react-native";
-import { styles, placeholderColor } from "./style";
+import { FlatList, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { styles } from "./style";
+import { Participant } from "../components/Participant";
 
 export function Home() {
   function handleParticipantAdd() {
     console.log("Você clicou no botão de Adicionar!");
   }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`Você clicou em remover o participante ${name}`);
+  }
+
+  const participants = [
+    "Maurício Mello",
+    "Pedro José",
+    "Leonardo Csik",
+    "Rodrigo Manguinho",
+    "Vini Black",
+    "Diego Fernandes",
+    "Biro Biro",
+    "Ana Clara",
+    "Isa Silva",
+    "Jack Bauer",
+    "Mayk Brito",
+    "João da Silva",
+  ];
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
 
       <View style={styles.form}>
-        <TextInput 
+        <TextInput
           style={styles.input}
           placeholder="Nome do participante"
           placeholderTextColor="#6B6B6B"
         />
 
         <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-          <Text style={styles.buttonText}>
-            +
-          </Text>
+          <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+
+      <FlatList 
+        data={participants}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Participant 
+            key={item} 
+            name={item} 
+            onRemove={() => handleParticipantRemove("Rodrigo")} 
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>
+            Ninguém chegou no evento ainda? Adicione participantes a sua lista de presença.
+          </Text>
+        )}
+      />
+      
     </View>
   );
 }
